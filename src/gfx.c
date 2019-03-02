b50bce78f1bf75ad24424b77cc6c2062228ce92b
James Hollister <jhollisterjr@gmail.com>
2019-03-01 21:56:10 -0800

    Keyboard input started

--- src/gfx.c
+++ src/gfx.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <SDL2/SDL_events.h>
 
 #include "chip8_gfx.h"
 #include "chip8.h"
@@ -8,13 +9,66 @@ static SDL_Renderer *render;
 
 void gfx_check_events(uint16_t *keys) {
     SDL_Event event;
+    *keys = 0;
     if (SDL_PollEvent(&event)) {
         switch (event.type) {
             case SDL_QUIT:
-                gfx_quit();
-                exit(EXIT_SUCCESS);
+                gfx_quit(EXIT_SUCCESS);
                 break;
             case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_1:
+                        *keys |= (1 << 0x1);
+                        break;
+                    case SDLK_2:
+                        *keys |= (1 << 0x2);
+                        break;
+                    case SDLK_3:
+                        *keys |= (1 << 0x3);
+                        break;
+                    case SDLK_4:
+                        *keys |= (1 << 0xc);
+                        break;
+                    case SDLK_q:
+                        *keys |= (1 << 0x4);
+                        break;
+                    case SDLK_w:
+                        *keys |= (1 << 0x5);
+                        break;
+                    case SDLK_e:
+                        *keys |= (1 << 0x6);
+                        break;
+                    case SDLK_r:
+                        *keys |= (1 << 0xd);
+                        break;
+                    case SDLK_a:
+                        *keys |= (1 << 0x7);
+                        break;
+                    case SDLK_s:
+                        *keys |= (1 << 0x8);
+                        break;
+                    case SDLK_d:
+                        *keys |= (1 << 0x9);
+                        break;
+                    case SDLK_f:
+                        *keys |= (1 << 0xe);
+                        break;
+                    case SDLK_z:
+                        *keys |= (1 << 0xa);
+                        break;
+                    case SDLK_x:
+                        *keys |= (1 << 0x0);
+                        break;
+                    case SDLK_c:
+                        *keys |= (1 << 0xb);
+                        break;
+                    case SDLK_v:
+                        *keys |= (1 << 0xf);
+                        break;
+                    case SDLK_ESCAPE:
+                        gfx_quit(EXIT_SUCCESS);
+                        break;
+                }
                 break;
             default:
                 SDL_Log("Unknow SDL event\n");
@@ -66,8 +120,9 @@ void gfx_update(uint8_t *disp_buf) {
     SDL_Delay(2);
 }
 
-void gfx_quit() {
+void gfx_quit(int code) {
     SDL_DestroyRenderer(render);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    exit(code);
 }
